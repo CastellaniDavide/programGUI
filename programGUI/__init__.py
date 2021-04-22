b1aa85e5d61119f116f27c9ef4caf9377839f6ee
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-04-22 20:10:38 +0200

    Created programGUI/__init__.py

---
+++ programGUI/__init__.py
@@ -0,0 +1,19 @@
+"""programGUI
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-4-22"
+
+class programGUI:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(programGUI.programGUI())
+	
+	def programGUI():
+		"""programGUI first funtion
+		"""
+		return "programGUI" #for the test (see test_programGUI.py)
+		
+if __name__ == "__main__":
+	programGUI()
